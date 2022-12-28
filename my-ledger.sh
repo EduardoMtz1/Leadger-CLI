d5360a335a356b1678cbb518cfdcaef05a618fb6
Eduardo Martinez <hbkeduardo10@hotmail.com>
2022-12-27 19:18:14 -0600

    Modified: Index file route

--- my-ledger.sh
+++ my-ledger.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-java Ledger  --price-db /Sample_files/prices_db \
--f /Sample_files/index.ledger "$@"
+java Ledger  --price-db ./Sample_files/prices_db \
+-f ./Sample_files/index.ledger "$@"
