9cc8ece62ee20aba8fd14f2f999ea1229b757128
Eduardo Martinez <hbkeduardo10@hotmail.com>
2022-12-29 20:35:07 -0600

    Added: Movement class, for the movements in a Registry

---
+++ Movement.java
@@ -0,0 +1,11 @@
+public class Movement {
+    public String name;
+    public double amount;
+    public String currency;
+
+    public Movement(String n, double a, String c){
+        name = n;
+        amount = a;
+        currency = c;
+    }
+}
